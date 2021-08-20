31956aed5a17987601646a280f71f2c6b0d27adb
Andrew Healey <doolie.healey@gmail.com>
2021-08-19 23:15:50 -0400

    Use local filenames for caching when possible

--- labelbox2coco.py
+++ labelbox2coco.py
@@ -8,6 +8,7 @@ import logging
 import requests
 from PIL import Image
 import numpy as np
+import os
 
 import rasterio
 import shapely
@@ -17,7 +18,7 @@ import time
 from tqdm import tqdm
 
 def labelbox_to_json(labeled_data, coco_output, images_output_dir,cat_order=None,verbose=False):
-    
+
     """
     Converts from labelbox json export format to COCO json
     stores the png files locally
@@ -26,7 +27,7 @@ def labelbox_to_json(labeled_data, coco_output, images_output_dir,cat_order=None
     Args:
         labeled_data: json file exported from labelbox
         coco output: path of json file to store the coco labels
-        images_output_dir: folder to put the 
+        images_output_dir: folder to put the
 
     """
 
@@ -58,36 +59,45 @@ def labelbox_to_json(labeled_data, coco_output, images_output_dir,cat_order=None
 
     # Go though each labelled image
 
+    existing_files=os.listdir(images_output_dir)
+
     for data in tqdm(label_data):
         im=None
-        for i in range(3):
-            try:
-                # Download and get image name
-                try:
-                    response = requests.get(data['Labeled Data'], stream=True)
-                except requests.exceptions.MissingSchema as e:
-                    logging.exception(('"Labeled Data" field must be a URL. '
-                                      'Support for local files coming soon'))
-                    continue
-                except requests.exceptions.ConnectionError as e:
-                    logging.exception('Failed to fetch image from {}'
-                                      .format(data['Labeled Data']))
-                    continue
-
-                response.raw.decode_content = True
-
-                # Open image and get image size
-                im = Image.open(response.raw)
-                break
-            except:
-                time.sleep(0.5)
-                continue
-
-        if im is None: continue
-        width, height = im.size
 
         # Create an id using consecutive numbers
-        image_id = len(coco['images']) + 1
+        image_id = data['External ID']
+
+        if image_id in existing_files:
+          im=Image.open(open(images_output_dir+"/"+image_id))
+        if im is None:
+          for i in range(3):
+              try:
+                  # Download and get image name
+                  try:
+                      response = requests.get(data['Labeled Data'], stream=True)
+                  except requests.exceptions.MissingSchema as e:
+                      logging.exception(('"Labeled Data" field must be a URL. '
+                                        'Support for local files coming soon'))
+                      continue
+                  except requests.exceptions.ConnectionError as e:
+                      logging.exception('Failed to fetch image from {}'
+                                        .format(data['Labeled Data']))
+                      continue
+
+                  response.raw.decode_content = True
+
+                  # Open image and get image size
+                  im = Image.open(response.raw)
+                  break
+              except:
+                  time.sleep(0.5)
+                  continue
+
+        if im is None:
+          print("No image for file",image_id,data['Labeled Data'])
+          continue
+        width, height = im.size
+
 
         # Print status
         if verbose: print('###### Processing ' + data['ID'] + ' image, ' + 'Image ' + str(count) + ' of ' +  str(len(label_data)))
@@ -96,7 +106,7 @@ def labelbox_to_json(labeled_data, coco_output, images_output_dir,cat_order=None
         # Write image in png format, is will have the ID as name (e.g. 23.png)
         image_path = images_output_dir + '/' + str(image_id) + '.png'
         im.save(image_path)
-        image_name = str(image_id) + '.png'
+        image_name = image_id
 
         # Include only images with annotations
         if not ('objects' in data['Label']):
@@ -125,7 +135,7 @@ def labelbox_to_json(labeled_data, coco_output, images_output_dir,cat_order=None
 
             # convert the masks into polygon format
             for category_name, binary_masks in labels.items():
-                            
+
                 polygons = []
 
                 count2 = 1
@@ -162,7 +172,7 @@ def labelbox_to_json(labeled_data, coco_output, images_output_dir,cat_order=None
                             logging.exception('Failed to fetch image from {}'
                                     .format(mask['instanceURI']))
                             return None
-                        
+
                         response.raw.decode_content = True
 
                         # Open the binary mask (it is just a png image with 1 and 0)
@@ -221,7 +231,7 @@ def labelbox_to_json(labeled_data, coco_output, images_output_dir,cat_order=None
         f.write(json.dumps(coco))
         if verbose: print("Image preprocessing ready")
 
-# 
+#
 def mask_to_polygons_layer(mask):
 
     """
@@ -234,7 +244,7 @@ def mask_to_polygons_layer(mask):
 
     # Use rasterio to generate shapes of pixels greater than 1 (I am using the first band)
     generator = rasterio.features.shapes(mask[:,:,0].astype(np.int16),connectivity=8, mask=(mask[:,:,0] >0),transform=rasterio.Affine(1.0, 0, 0, 0, 1.0, 0))
-    
+
     all_polygons = []
 
     # Put all the polygons in a list
@@ -242,3 +252,4 @@ def mask_to_polygons_layer(mask):
         all_polygons.append(shapely.geometry.shape(poly))
 
     return all_polygons
+
